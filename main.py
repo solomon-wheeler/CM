9d1cc30fea02ac01baac6a8d77f96458860ff9b7
Solly <solly.wheeler@gmail.com>
2021-10-14 17:15:08 +0100

    Cleaning up code + commenting

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-class node():
+class node:
     def __init__(self, m_wrong, c_wrong, boat_wrong, parent):
         self.state = [m_wrong, c_wrong, boat_wrong]
         self.parent = parent
@@ -19,30 +19,23 @@ class node():
         new_state = [self.state[0] - action[0], self.state[1] - action[1], self.state[2] - action[2]]
         other_side = [3 - new_state[0], 3 - new_state[1]]
         print(other_side)
-        # checking if vaild, saves spaces from creating then calling is valid function
-        if new_state[0] < new_state[1]:
+        # checking if valid *before* creating saves memory in comparison to creating then running an isValid() function
+        if new_state[0] < new_state[1]:  # checking if more cannibals than missionaries on wrong side
             if new_state[0] != 0:
                 return False
-        if other_side[0] < other_side[1]:
+        if other_side[0] < other_side[1]: #checking if more cannibales than missionaries on correct side
             if other_side[0] != 0:
                 return False
-        if new_state[0] < 0 or new_state[1] < 0 or new_state[2] < 0:
+        if new_state[0] < 0 or new_state[1] < 0 or new_state[2] < 0: # checking if we have enough canibals/missionaries on the side to make the move. Boat check should never run, but is useful to catch bugs
             return False
-        if new_state[0] > 3 or new_state[1] > 3 or new_state[2] > 3:
+        if new_state[0] > 3 or new_state[1] > 3 or new_state[2] > 3: # ^ on other side
             return False
 
         return node(self.state[0] - action[0], self.state[1] - action[1], self.state[2] - action[2],
                     parent)  # don't need else here because this will only run of no false has been returned
 
-    # def is_valid_state(self): #not sure if i need this
-    #   if self.state[0] < self.state[1]:
-    #      return True
-    # if self.state[0] or self.state[1] or self.state[]
-    # else:
-    #   return False
 
-
-class game():
+class game:
     def __init__(self):
         self.initial_node = node(3, 3, 1, None)
         self.frontier = []
@@ -50,48 +43,38 @@ class game():
         self.possible_moves_boat = [[1, 0, 1], [2, 0, 1], [0, 1, 1], [0, 2, 1], [1, 1, 1]]
         self.possible_moves_no_boat = [[-1, 0, -1], [-2, 0, -1], [0, -1, -1], [0, -2, -1], [-1, -1, -1]]
 
-    # def add_children(self):
-    #   for this_try in possible_moves:
-    #      self.current_state.get_child_node()
     def breadth_first_search(
             self):  # pseudo from Rusell and Norvig Artificial Intelligence: A modern approach 4th ed p.82
         self.current_state = self.initial_node
 
         while self.current_state.is_goal_state() == False:  # creating_children for node
             self.explored.append(self.current_state)
-            # print(str(self.current_state.get_state()[2]) + "h")
-            # boat = 0
             state = self.current_state.get_state()
-            if state[2] == 1:
+            if state[2] == 1: #checking which side the boat is on, as this dictates how we need to change the state
                 moves_to_try = self.possible_moves_boat
             else:
                 moves_to_try = self.possible_moves_no_boat
-            # else:
-            #   boat = 0
-            for this_try in moves_to_try:
-
-                # print(this_try)
+            for this_try in moves_to_try: #going through all of the possible moves
                 returned_child = self.current_state.get_child_node(this_try, self.current_state)
                 if returned_child != False:
                     already_found = False
-                    for x in self.frontier:
+                    for x in self.frontier: #checking if this state is already in the frontier
                         if x.get_state() == returned_child.get_state():
                             already_found = True
-                    for x in self.explored:
+                    for x in self.explored: #checking if this state has already been explored
                         if x.get_state() == returned_child.get_state():
                             already_found = True
                     if already_found == False:
                         self.frontier.append(returned_child)
-                        print(str(returned_child.state))
-            print(self.frontier)
-            if len(self.frontier) == 0:
+            if len(self.frontier) == 0: #nothing in frontier means we have nothing left to explore, so we have finished our search
                 return "not found"
-            self.current_state = self.frontier.pop(0)
+            self.current_state = self.frontier.pop(0) #breadth first uses a FIFO data structure, emulated here.
 
         return self.current_state
 
 
 ##main##
+
 overall_game = game()
 result = overall_game.breadth_first_search()
 if result == "not found":
@@ -99,6 +82,6 @@ if result == "not found":
 else:
     print("found result" + str(result.get_state()))
     parent = result.get_parent()
-    while parent != None:
+    while parent != None: #going through all parent's to show moves
         print(parent.get_state())
         parent = parent.get_parent()
